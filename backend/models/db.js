bfb79f513d3a8f6ad0609b3de1ee222b2c7803c0
name <email>
2024-06-30 09:02:29 +0530

    config changes

--- backend/models/db.js
+++ backend/models/db.js
@@ -1,14 +1,12 @@
-
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'latex_management'
+  host: "latex-collecting.cv6k2qqyuwkp.ap-southeast-2.rds.amazonaws.com",
+  user: "admin",
+  password: "LatexMnage12",
+  database: "latex_management",
 });
 
 console.log("db");
 
 module.exports = pool.promise();
-
